632a26abb6a27934367e306d431f01d6784a627f
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-12-10 00:09:10 +0530

    routes

--- MovieApp/js/app.js
+++ MovieApp/js/app.js
@@ -1,5 +1,25 @@
   var app = angular.module("myApp",["ngRoute"]);
 
+//factory
+app.factory('GetData',function($http){
+  return{
+    movieData: function(type,passdata,response){
+      var urlLink = "https://api.cinemalytics.com/v1/movie/" + "/" +passdata+ ""
+    }
+  }
+})
+
+
+
+//controller
+app.controller('allMovie',function($scope,GetData,$routeProvider,$route,$location){
+
+});
+
+//controller
+app.controller('singleMovie',function($scope,GetData,$routeProvider,$route,$location){
+
+});
   app.config(function($routeProvider){
   	$routeProvider
   	.when("/year/:year",{
@@ -7,7 +27,7 @@
   		controller :"allMovie"
   	})
 
-  	.when("/year/:year",{
+  	.when("/id/:ID",{
   		templateUrl : "template/single.html",
   		controller : "singleMovie"
   	})
