23f7fa814142967f907b7a0aa7a122b640f9a138
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-12-10 08:42:11 +0530

    call to factory from controller for taking movie data

--- MovieApp/js/app.js
+++ MovieApp/js/app.js
@@ -3,8 +3,10 @@
 //factory
 app.factory('GetData',function($http){
   return{
+    //get data of movies from api
     movieData: function(type,passdata,response){
       var urlLink = "https://api.cinemalytics.com/v1/movie/" + "/" +passdata+ ""
+      $http.get(urlLink).success(response);
     }
   }
 })
@@ -14,6 +16,22 @@ app.factory('GetData',function($http){
 //controller
 app.controller('allMovie',function($scope,GetData,$routeProvider,$route,$location){
 
+  $scope.year = $routeParams.year;
+
+//call to factory for movie data
+  GetData.movieData('year',$routeParams.year,function(response){
+    $scope.movie = response;
+  });
+
+  //redirect to page
+  $scope.getMovie = function(id){
+    $location.path("/id/" + id);
+  };
+
+  ///redirect to page
+   $scope.getMovie = function(year){
+    $location.path("/year/" + year);
+  };
 });
 
 //controller
