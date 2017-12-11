d5ca2d31195c00f951f283a873c4551a18e7de76
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-12-11 16:54:44 +0530

    Game completed

--- Game/js/app.js
+++ Game/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("gameApp",[]);
 
-app.controller("GameController",function($scope){
+app.controller("GameController",function($scope,$timeout){
 
 	var words = ['cat' , 'rat' , 'bat' , 'mat'];
 	$scope.incorrectLettersChosen = [];
@@ -37,7 +37,58 @@ app.controller("GameController",function($scope){
 	}
 
 	$scope.letterChosen = function(){
-			
+		//if user enters correct letter again
+			for (var i = 0; i < $scope.correctLettersChosen.length; i++) {
+				if($scope.correctLettersChosen[i].toLowerCase() == $scope.input.letter.toLowerCase()){
+					$scope.input.letter = "";
+					return;
+				}
+			}
+
+		//if user enters correct letter again
+			for (var i = 0; i < $scope.incorrectLettersChosen.length; i++) {
+				if($scope.incorrectLettersChosen[i].toLowerCase() == $scope.input.letter.toLowerCase()){
+					$scope.input.letter = "";
+					return;
+				}
+			}
+
+			var correct = false;
+			//if entered letter is correct
+			for (var i = 0; i <selectedWord.length; i++) {
+				if(selectedWord[i].toLowerCase() == $scope.input.letter.toLowerCase()){
+					//make correct letter appear as letter instead of star
+					$scope.displayWord = $scope.displayWord.slice(0,i)+$scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1);
+					correct = true;
+				}
+			}
+
+			//update chosen arrays
+			if(correct){
+				$scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+			}
+			else{
+				$scope.guesses--;
+				$scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase())
+			}
+
+			$scope.input.letter = "";
+
+			//if all gusses are over
+			if($scope.guesses == 0){
+				$timeout(function(){
+					newGame();
+				},500);
+			}
+
+			//if all letters are guessed correctly
+			if($scope.displayWord.indexOf("*")==-1){
+				$timeout(function(){
+					newGame();
+				},500);
+			}
+
+
 	}
 
 	newGame();
