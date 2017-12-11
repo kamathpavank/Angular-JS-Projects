95b874cfe33c8448ac71f46447c28b22d8ec796c
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-12-11 13:11:11 +0530

    random words in star form

---
+++ Game/js/app.js
@@ -0,0 +1,40 @@
+var app = angular.module("gameApp",[]);
+
+app.controller("GameController",function($scope){
+
+	var words = ['cat' , 'rat' , 'bat' , 'mat'];
+	$scope.incorrectLettersChosen = [];
+	$scope.correctLettersChosen = [];
+	$scope.guesses = 6;
+	$scope.displayWord='';
+	$scope.input = {
+		letter : ''
+	}
+
+	//function to select random words
+	var selectRandomWord = function(){
+		var index = Math.round(Math.random() * words.length);
+		return words[index];
+	}
+
+		//function for new game
+	var newGame = function(){
+
+		$scope.incorrectLettersChosen = [];
+		$scope.correctLettersChosen = [];
+		$scope.guesses = 6;
+		$scope.displayWord='';
+
+		selectedWord = selectRandomWord();
+		var tempDisplayWord = '';
+
+		//loop to represent word in stars
+		for (var i = 0; i < selectedWord.length; i++) {
+			tempDisplayWord +='*';
+
+		}
+		$scope.displayWord = tempDisplayWord;
+	}
+
+	newGame();
+});
